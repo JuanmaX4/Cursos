92769ca20278371fbb9e5d202911295b14e82c4d
JuanmaX4 <74423956+JuanmaX4@users.noreply.github.com>
2022-07-16 17:36:40 +0200

    Update Contador con ReactJS

---
+++ JavaScript/contador/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div className="main">
+      <h1 className={`${counter < 0 && "negative"}`}>{counter}</h1>
+      <div className="btn">
+        <button id="decrease" onClick={() => setCounter(prev => prev - 1)}>Decrease</button>
+        <button id="reset" onClick={() => setCounter(0)}>Reset</button>
+        <button id="increase" onClick={() => setCounter(prev => prev + 1)}>Increase</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
